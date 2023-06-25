22a4329cd478b25dc06d8e7bfe9a68abc7b90a30
Kyle Turner <283362+kyleturner@users.noreply.github.com>
2023-06-24 19:35:33 -0700

    Fixing Netlify adapter type

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
+  output: 'hybrid',
   adapter: netlify(),
   build: {
     rollupOptions: {
