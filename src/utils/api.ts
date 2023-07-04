8137bf0c8571dd1389cddc0689c2fec38853e4f1
Kyle Turner <283362+kyleturner@users.noreply.github.com>
2023-07-04 08:13:55 -0700

    Updating API for speed

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
 import { useStoryblokApi, ISbStoryData } from '@storyblok/astro'
 
 const isShowingDrafts = (import.meta.env.DEV || import.meta.env.SHOW_DRAFT_EXPERIENCES)
+const storyblok = useStoryblokApi()
 
 export async function getExperiences() {
-    const storyblok = useStoryblokApi();
     const { data } = await storyblok.get("cdn/stories", {
         content_type: "experience",
         version: isShowingDrafts ? "draft" : "published",
@@ -12,7 +12,6 @@ export async function getExperiences() {
 }
 
 export async function getExperienceStories() {
-    const storyblok = useStoryblokApi();
     const response = await storyblok.getStories( {
         version: isShowingDrafts ? "draft" : "published"
     })
@@ -21,8 +20,7 @@ export async function getExperienceStories() {
 }
 
 export async function getExperienceStory(slug) {
-    const sbApi = useStoryblokApi()
-    const { data } = await sbApi.get(`cdn/stories/${slug}`, {
+    const { data } = await storyblok.get(`cdn/stories/${slug}`, {
         version: isShowingDrafts ? "draft" : "published",
     })
     return data.story
