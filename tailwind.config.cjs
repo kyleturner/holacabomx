7c95cdd94d9d21fe0dff7c46f0c9509577ad02cf
Kyle Turner <283362+kyleturner@users.noreply.github.com>
2023-07-30 07:43:35 -0700

    Filters button design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        primary: '#2a9d8f',
+        primaryLight: '#7ab3ad',
+        primaryDark: '#264653',
+      },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
